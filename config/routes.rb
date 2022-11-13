23e9faa51a4470af4f678598d600e6f09842d5f9
EC2 Default User <ec2-user@ip-172-31-8-82.ap-northeast-1.compute.internal>
2022-11-13 19:04:47 +0000

    管理者側routes記述

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,14 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
 # 会員側
-
+ 
 # 管理者側
  namespace :admin do
+     resources :customers, only: [:index, :show, :edit, :update]
+     resources :revies, only: [:index, :show]
+     resources :comments, only: [:index, :show, :destroy]
+     resources :home_appliances
+     resources :genres, only: [:new, :create, :edit, :update]
  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
