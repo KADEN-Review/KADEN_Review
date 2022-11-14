79b3546cce0c758fbff11223dc4beb811b0a2d67
EC2 Default User <ec2-user@ip-172-31-8-82.ap-northeast-1.compute.internal>
2022-11-14 17:06:14 +0000

    管理者側顧客一覧作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,31 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
+# 管理者用
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
 # 会員用
 devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
 
-# 管理者用
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
-# 会員側
- 
 # 管理者側
  namespace :admin do
      resources :customers, only: [:index, :show, :edit, :update]
      resources :revies, only: [:index, :show]
      resources :comments, only: [:index, :show, :destroy]
      resources :home_appliances
-     resources :genres, only: [:new, :create, :edit, :update]
+     resources :genres, only: [:index, :create, :edit, :update]
  end
+
+# 会員側
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
